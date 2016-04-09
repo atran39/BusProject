4b1e9ffc8230cfe132c0a925e0c4c91bc90e3fc1
Vermanator <shawn.verma@gmail.com>
2016-04-09 15:19:38 -0400

    Update Route.java

--- Route.java
+++ Route.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
 public class Route {
 	private ArrayList<String> stops = new ArrayList<String>();
 	private String name;
-	private final static int quotesNum=5;
-	public Route(String route) {
-
+	private String routeID;
+	private final static int NUMBER_OF_QUOTES=5;
+	public Route(String route) throws IOException {
+		routeID=routeToRouteID(route);
+		stops = routeArrayMaker();
+		System.out.print(stops.toString());
 	}
 
-	public Route() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public ArrayList<String> getStops() {
 		return stops;
@@ -27,11 +27,34 @@ public class Route {
 		return name;
 	}
 
-	public static String routeToRouteID(String route) throws IOException {
+	static String routeToRouteID(String route) throws IOException {
 	String routes = httpGet("http://api.umd.io/v0/bus/routes");
-	return keyWordExtract(route, routes);
+	return keyWordExtract(route, routes, NUMBER_OF_QUOTES);
 	}
-
+	private ArrayList<String> routeArrayMaker() throws IOException {
+		ArrayList<String> stopList = new ArrayList<String>();
+		boolean isKey = true;
+		String stops = httpGet("http://api.umd.io/v0/bus/routes/"+routeID);
+		String keyWord = "\"stop_id\":\"";
+		for (int i = 0; i < stops.length() - keyWord.length()+1; i++) {
+			for (int j = 0; j < keyWord.length(); j++) {
+				if (!(stops.charAt(i + j) == keyWord.charAt(j))) {
+					isKey = false;
+				}		
+			}
+			if(isKey) {
+				String id = "";
+				int counter = i + keyWord.length();
+				while (stops.charAt(counter) != '"') {
+					id += stops.charAt(counter);
+					counter++;
+				}
+				stopList.add(id);
+			}
+			isKey=true;
+		}
+		return stopList;
+	}
 	private static int indexOfKeyWord(String keyWord, String fullString) {
 		boolean isKey = true;
 		for (int i = 0; i < fullString.length(); i++) {
@@ -47,13 +70,12 @@ public class Route {
 		}
 		return -1;
 	}
-	static String keyWordExtract(String keyWord, String fullString) {
+	static String keyWordExtract(String keyWord, String fullString, int quotesNum) {
 		int keyWordIndex= indexOfKeyWord(keyWord,fullString);
 		int startingIndex=keyWordIndex;
-		int i=quotesNum;
-		while(i>0) {
+		while(quotesNum>0) {
 			if(fullString.charAt(startingIndex)=='"'){
-				i--;
+				quotesNum--;
 			}
 			startingIndex--;
 		}
